4e0d06dc0dc516008480b751966c4a86db8c3017
hihayk <say@hihayk.com>
2018-03-21 18:11:07 +0100

    setup react and babel with kitt

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Button from '@typeform/kitt/lib/components/button'
+
+const App = () => (
+  <div>
+    Hello
+    <Button>
+      hey ho
+    </Button>
+  </div>
+)
+
 console.log('hello world')
+
+render(<App />, document.getElementById('root'))
