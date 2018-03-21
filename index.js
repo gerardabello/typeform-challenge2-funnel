43a9923d4b945f70ba7a8f0d5b5bc1e57a90122e
Gerard Abello Serras <gerardabello@gmail.com>
2018-03-21 16:31:39 +0100

    new tracking

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ var app = express()
 
 const PORT = 3000
 
-/*
- {
-  sessionId: {timeStart: int, timeEnd: int, lastSeenBlockRef: string}
- }
- */
-let db = {}
+let db = []
 
 app.use(bodyParser.text())
 
@@ -31,44 +26,17 @@ app.get('/data', function (req, res) {
 })
 
 // TRACKING ENDPOINTS
-app.post('/load', function (req, res) {
+app.post('/track', function (req, res) {
   const body = JSON.parse(req.body)
-  const sessionId = body.sessionId
 
-  db[sessionId] = { timeStart: new Date().getTime() }
-
-  res.end()
-})
-
-app.post('/unload', function (req, res) {
-  const body = JSON.parse(req.body)
-  const sessionId = body.sessionId
-
-  if (!db[sessionId]) {
-    return
-  }
-
-  db[sessionId] = Object.assign({}, db[sessionId], {
-    timeEnd: new Date().getTime(),
-    lastSeenBlockRef: body.lastSeenBlockRef
+  db.push({
+    type: body.blockType,
+    formId: body.formId,
+    sessionId: body.sessionId,
+    blockRef: body.blockRef,
+    timestamp: new Date().getTime()
   })
 
-  res.end()
-})
-
-app.post('/submit', function (req, res) {
-  const body = JSON.parse(req.body)
-  const sessionId = body.sessionId
-
-  if (!db[sessionId]) {
-    console.log('Unknown sessionId ' + sessionId)
-    return
-  }
-
-  db[sessionId] = Object.assign({}, db[sessionId], {
-    timeEnd: new Date().getTime(),
-    lastSeenBlockRef: 'submit'
-  })
   res.end()
 })
 
