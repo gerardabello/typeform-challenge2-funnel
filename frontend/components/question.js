00fbe98305a1951ccd0be96c372ff06aac997b4b
hihayk <say@hihayk.com>
2018-03-22 15:34:25 +0100

    required icon

--- frontend/components/question.js
+++ frontend/components/question.js
@@ -5,15 +5,36 @@ import Text from '@typeform/kitt/lib/components/text'
 import Spacer from '@typeform/kitt/lib/components/spacer'
 import Block from '@typeform/kitt/lib/components/block'
 import ChartBar from './chart-bar'
+import styled from 'styled-components'
+import { colors } from '@typeform/kitt/lib/variables'
 
-const Question = ({ title, dropoutsAmount, visitsAmount, blockType, blockIndex }) => (
+const BlockWrapper = styled.div`
+  position: relative;
+`
+const RequiredWrapper = styled.div`
+  position: absolute;
+  right: 100%;
+  top: 0;
+  width: 32px;
+`
+
+const Question = ({ title, dropoutsAmount, visitsAmount, blockType, blockIndex, isRequired }) => (
   <Split>
     <SplitItem padRight={1.5}>
-      <Block
-        blockType={blockType}
-        size='smWide'
-        blockIndex={blockIndex}
-      />
+      <BlockWrapper>
+        <Block
+          blockType={blockType}
+          size='smWide'
+          blockIndex={blockIndex}
+        />
+        {isRequired &&
+          <RequiredWrapper>
+            <Text color={colors.error} align='center' size='size2'>
+              *
+            </Text>
+          </RequiredWrapper>
+        }
+      </BlockWrapper>
     </SplitItem>
     <SplitItem width='remaining' padRight={8}>
       <Spacer bottom={1}>
